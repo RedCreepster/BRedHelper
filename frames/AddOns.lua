1e36bb24639c8a843e10b324b467c6070268a95d
RedCreepster <redcreepster@gmail.com>
2018-01-07 17:23:28 +0300

    Initial commit with sources.

---
+++ frames/AddOns.lua
@@ -0,0 +1,231 @@
+﻿events = {
+    'ADDON_LOADED',
+    'BAG_UPDATE_DELAYED',
+    'COMPACT_UNIT_FRAME_PROFILES_LOADED',
+    'CURRENCY_DISPLAY_UPDATE',
+    'MERCHANT_CLOSED',
+    'MERCHANT_SHOW',
+    'PLAYER_ALIVE',
+    'PLAYER_TARGET_CHANGED',
+    'PRODUCT_CHOICE_UPDATE',
+    'PRODUCT_DISTRIBUTIONS_UPDATED',
+    'STORE_PURCHASE_LIST_UPDATED',
+    'STORE_STATUS_CHANGED',
+    'TABARD_CANSAVE_CHANGED',
+    'TOKEN_DISTRIBUTIONS_UPDATED',
+    'TOKEN_STATUS_CHANGED',
+    'UNIT_FACTION',
+    'UNIT_MODEL_CHANGED',
+    'UNIT_NAME_UPDATE',
+    'UNIT_TARGET',
+    'UNIT_THREAT_LIST_UPDATE',
+    'UNIT_THREAT_SITUATION_UPDATE',
+};
+
+enabledAddOns = {
+    ['World'] = { 'RedHelper' },
+    ['Instance'] = { 'RedHelper' },
+};
+
+debug = false;
+
+local MAP_TYPES = {
+    'World',
+    'Instance',
+};
+local selectedMapType = MAP_TYPES[1];
+
+local addonsItemsToNames = {};
+
+function log(...)
+    if debug then
+        print(...);
+    end
+end
+
+function init()
+    Frame1:SetScript('OnEvent', function(_, event)
+        if event == 'PLAYER_LOGIN' then
+            run();
+            print('RedHelper loaded! For open addons window use command /rehelper')
+        elseif event == 'PLAYER_ENTERING_WORLD' then
+            if needReload() then
+                Frame2:Show();
+            end
+        end
+    end);
+    Frame1:RegisterEvent('PLAYER_LOGIN');
+    Frame1:RegisterEvent('PLAYER_ENTERING_WORLD');
+end
+
+--- Создаёт контейнер с чекбоксом и текстом лля аддона
+-- @param name
+-- @param enable
+--
+function createAddonItem(name, enable)
+    local namePrefix = 'addon-' .. name;
+
+    local addonContainer = CreateFrame('Frame', namePrefix .. '-container');
+
+    local checkButton = createCheckButton(namePrefix .. '-checkbutton', addonContainer, nil, enable, { 'LEFT' });
+    local textPoint = { 'LEFT', checkButton, checkButton:GetWidth(), 0 }
+    local text = createText(namePrefix .. '-text', addonContainer, name, textPoint);
+
+    addonContainer:SetWidth(checkButton:GetWidth() + text:GetWidth());
+    addonContainer:SetHeight(max(checkButton:GetHeight(), text:GetHeight()));
+
+    return addonContainer, checkButton, text;
+end
+
+function run()
+    local addonFrameHeight = 23;
+
+    local addonsContainer = CreateFrame('Frame', 'addons-container', ScrollFrame1);
+    addonsContainer:SetPoint('TOPLEFT', ScrollFrame1);
+    ScrollFrame1:SetScrollChild(addonsContainer);
+
+    local addons = getAddons();
+
+    local addonsContainerHeight = ((#addons) + 1) * addonFrameHeight;
+
+    addonsContainer:SetHeight(addonsContainerHeight);
+    addonsContainer:SetWidth(ScrollFrame1:GetWidth());
+
+    for index, addon in ipairs(addons) do
+        local addonName = addon['name'];
+
+        local addonItem, checkButton = createAddonItem(addon['title'], addon['enabled']);
+
+        addonsItemsToNames[addonItem] = addonName;
+
+        local point = {
+            'TOPLEFT',
+            addonsContainer,
+            0,
+            (index - 1) * -addonFrameHeight
+        };
+        setParams(addonItem, point);
+        addonItem:SetParent(addonsContainer);
+
+        checkButton:SetScript('OnClick', function(self)
+            if not enabledAddOns[selectedMapType] then
+                enabledAddOns[selectedMapType] = {};
+            end
+
+            local selectedEnabledAddons = enabledAddOns[selectedMapType];
+
+            local contains = tContains(selectedEnabledAddons, addonName);
+
+            if self:GetChecked() and not contains then
+                tinsert(selectedEnabledAddons, addonName);
+            elseif contains then
+                vtremove(selectedEnabledAddons, addonName);
+            end
+        end);
+    end
+
+    updateCheckboxes();
+
+    createSimpleMenu(DropDownMenuTest, MAP_TYPES, 1, function(_, name)
+        selectedMapType = name;
+        updateCheckboxes();
+    end);
+end
+
+function updateCheckboxes()
+    local _, addonsContainer = ScrollFrame1:GetChildren();
+
+    local addonsItems = { addonsContainer:GetChildren() };
+
+    for _, addonItem in ipairs(addonsItems) do
+        local children = addonItem:GetChildren();
+        if children then
+            local addonName = addonsItemsToNames[addonItem];
+            children:SetChecked(enabledAddOns[selectedMapType] and tContains(enabledAddOns[selectedMapType], addonName));
+        end
+    end
+end
+
+function needReload()
+    local type = getCurrentMapType();
+
+    if not enabledAddOns[type] then
+        return false;
+    end
+
+    local addons = getAddons();
+
+    for _, addon in ipairs(addons) do
+        local addonName = addon['name'];
+        local enabled = addon['enabled'];
+
+        if tContains(enabledAddOns[type], addonName) then
+            if not enabled then
+                return true;
+            end
+        else
+            if enabled then
+                return true;
+            end
+        end
+    end
+
+    return false;
+end
+
+function applyAddOns()
+    local type = getCurrentMapType();
+    local addons = getAddons();
+
+    local needReload = false;
+
+    for _, addon in ipairs(addons) do
+        local addonName = addon['name'];
+        local enabled = addon['enabled'];
+
+        if (not enabledAddOns[type] or not tContains(enabledAddOns[type], addonName)) and enabled then
+            DisableAddOn(addonName);
+            log('Disabled ' .. addonName);
+            if IsAddOnLoaded(addonName) then
+                needReload = true;
+            end
+        end
+    end
+
+    for _, addon in ipairs(addons) do
+        local addonName = addon['name'];
+        local enabled = addon['enabled'];
+        local demand = addon['demand'];
+
+        if enabledAddOns[type] and tContains(enabledAddOns[type], addonName) and not enabled then
+            EnableAddOn(addonName);
+            log('Enabled ' .. addonName);
+
+            if not needReload and not IsAddOnLoaded(addonName) and not demand then
+                LoadAddOn(addonName);
+                log('Loaded ' .. addonName);
+            end
+        end
+    end
+
+    if needReload then
+        ReloadUI();
+    end
+end
+
+--- Возвращает тип текущей карты.
+--
+-- World - Мир, Instance - Подземелье, Рейд, Поле боя, Арена, etc...
+--
+function getCurrentMapType()
+    if IsInInstance() then
+        return 'Instance';
+    else
+        return 'World';
+    end
+end
+
+SLASH_REDHELPER1 = '/redhelper';
+function SlashCmdList.REDHELPER()
+    Frame1:Show();
+end
---
+++ utils.lua
@@ -0,0 +1,172 @@
+﻿function getAddOn(nameOrIndex)
+    local name, title, notes, loadable, reason, security = GetAddOnInfo(nameOrIndex);
+
+    return {
+        ["name"] = name,
+        ["title"] = title,
+        ["notes"] = notes,
+        ["enabled"] = GetAddOnEnableState(nil, nameOrIndex) > 0,
+        ["loaded"] = IsAddOnLoaded(nameOrIndex),
+        ["loadable"] = loadable,
+        ["demand"] = IsAddOnLoadOnDemand(nameOrIndex),
+        ["reason"] = reason,
+        ["security"] = security
+    };
+end
+
+function printAddoInnfo(addon)
+    print('name: ' .. addon['name']);
+    print('title: ' .. addon['title']);
+    print('notes: ' .. toString(addon['notes']));
+    print('enabled: ' .. toString(addon['enabled']));
+    print('loaded: ' .. toString(addon['loaded']));
+    print('loadable: ' .. toString(addon['loadable']));
+    print('demand: ' .. toString(addon['demand']));
+    print('reason: ' .. toString(addon['reason']));
+    print('security: ' .. toString(addon['security']));
+end
+
+--- Получение списка аддонов
+--
+function getAddons()
+    local count = GetNumAddOns();
+
+    local addons = {};
+
+    for index = 1, count, 1 do
+        addons[index] = getAddOn(index);
+    end
+
+    return addons;
+end
+
+function setParams(frame, point, width, height)
+    if point then
+        frame:SetPoint(unpack(point));
+    end
+    if width then
+        frame:SetWidth(width);
+    end
+    if height then
+        frame:SetHeight(height);
+    end
+end
+
+function createButton(name, parent, text, point, width, height)
+    local button = CreateFrame("Button", name, parent, "UIPanelButtonTemplate");
+
+    setParams(button, point, width, height);
+
+    button:SetText(text);
+
+    return button;
+end
+
+function createCheckButton(name, parent, tooltip, checked, point, width, height)
+    local button = CreateFrame("CheckButton", name, parent, "ChatConfigCheckButtonTemplate");
+
+    setParams(button, point, width, height);
+
+    button.tooltip = tooltip;
+    button:SetChecked(checked);
+
+    return button;
+end
+
+function createText(name, parent, text, point, width, height)
+    local fontString = parent:CreateFontString(name, "BACKGROUND");
+
+    setParams(fontString, point, width, height);
+
+    fontString:SetFontObject("GameFontNormal");
+
+    fontString:SetText(text);
+
+    return fontString;
+end
+
+function setScrollFrameChilds(scrollFrame, containerName, childs)
+    local container = CreateFrame('Frame', containerName, scrollFrame);
+
+    container:SetWidth(scrollFrame:GetWidth());
+    container:SetPoint('TOPLEFT', scrollFrame);
+    scrollFrame:SetScrollChild(container);
+
+    for _, child in ipairs(childs) do
+        child:SetParent(container);
+    end
+
+    return container;
+end
+
+--- Создание простого меню
+-- @param menuButton Элеммент для открытия меню
+-- @param items Тектовый массив/таблица
+-- @param selectedID Первоначальное выбранное значение
+-- @param onChange
+-- @param width
+-- @param buttonWidth
+--
+function createSimpleMenu(menuButton, items, selectedID, onChange, width, buttonWidth)
+    UIDropDownMenu_Initialize(menuButton, function(_, level)
+        local info = UIDropDownMenu_CreateInfo();
+        for _, v in ipairs(items) do
+            info = UIDropDownMenu_CreateInfo();
+            info.text = v
+            info.value = v
+            info.func = function(self)
+                local id = self:GetID();
+                UIDropDownMenu_SetSelectedID(menuButton, id);
+                if onChange then
+                    onChange(id, items[id]);
+                end
+            end
+            UIDropDownMenu_AddButton(info, level);
+        end
+    end)
+    UIDropDownMenu_SetWidth(menuButton, width or 100);
+    UIDropDownMenu_SetButtonWidth(menuButton, buttonWidth or 124);
+    UIDropDownMenu_SetSelectedID(menuButton, selectedID);
+    UIDropDownMenu_JustifyText(menuButton, "LEFT");
+end
+
+function indexOf(table, item)
+    local index;
+
+    for i, currentItem in ipairs(table) do
+        if currentItem == item then
+            index = i;
+            break;
+        end
+    end
+
+    return index;
+end
+
+function vtremove(table, value)
+    local index = indexOf(table, value);
+    while (index) do
+        tremove(table, index);
+        index = indexOf(table, value);
+    end
+end
+
+function bool2String(value)
+    if value then
+        return 'true';
+    else
+        return 'false';
+    end
+end
+
+function toString(value)
+    if type(value) == 'boolean' then
+        return bool2String(value);
+    end
+
+    if value == nil then
+        return 'nil';
+    end
+
+    return value;
+end
